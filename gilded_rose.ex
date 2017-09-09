8d14faa241504ae9fd32eff848aa1238e9c74cbf
Sam Livingston-Gray <geeksam@gmail.com>
2017-09-08 17:44:41 -0700

    Extract clause for backstage passes

--- gilded_rose.ex
+++ gilded_rose.ex
@@ -16,6 +16,27 @@ defmodule GildedRose do
     |> update_item_sell_in
   end
 
+  defp update_item(item = %Item{ name: "Backstage passes to a TAFKAL80ETC concert" }) do
+    if item.quality < 50 do
+      item = %{item | quality: item.quality + 1}
+      if item.sell_in < 11 do
+        if item.quality < 50 do
+          item = %{item | quality: item.quality + 1}
+        end
+      end
+      if item.sell_in < 6 do
+        if item.quality < 50 do
+          item = %{item | quality: item.quality + 1}
+        end
+      end
+    end
+    item = %{item | sell_in: item.sell_in - 1}
+    if item.sell_in < 0 do
+      item = %{item | quality: item.quality - item.quality}
+    end
+    item
+  end
+
   defp quality_modifier( item = %Item{ name: "Aged Brie", sell_in: sell_in} ) when sell_in <= 0 do ; 2 ; end
   defp quality_modifier( item = %Item{ name: "Aged Brie" } )                                    do ; 1 ; end
 
@@ -36,35 +57,13 @@ defmodule GildedRose do
   end
 
   defp update_item(item) do
-    if item.name != "Backstage passes to a TAFKAL80ETC concert" do
-      if item.quality > 0 do
-        item = %{ item | quality: item.quality - 1 }
-      end
-    else
-      if item.quality < 50 do
-        item = %{item | quality: item.quality + 1}
-        if item.name == "Backstage passes to a TAFKAL80ETC concert" do
-          if item.sell_in < 11 do
-            if item.quality < 50 do
-              item = %{item | quality: item.quality + 1}
-            end
-          end
-          if item.sell_in < 6 do
-            if item.quality < 50 do
-              item = %{item | quality: item.quality + 1}
-            end
-          end
-        end
-      end
+    if item.quality > 0 do
+      item = %{ item | quality: item.quality - 1 }
     end
     item = %{item | sell_in: item.sell_in - 1}
     if item.sell_in < 0 do
-      if item.name != "Backstage passes to a TAFKAL80ETC concert" do
-        if item.quality > 0 do
-          item = %{item | quality: item.quality - 1}
-        end
-      else
-        item = %{item | quality: item.quality - item.quality}
+      if item.quality > 0 do
+        item = %{item | quality: item.quality - 1}
       end
     end
     item
