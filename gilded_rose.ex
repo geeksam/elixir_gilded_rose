52875f1bef3b33fb0fed8d02c738687047715003
Sam Livingston-Gray <geeksam@gmail.com>
2017-09-11 09:44:59 -0700

    Introduce typed structs, pattern match on them instead of item name

--- gilded_rose.ex
+++ gilded_rose.ex
@@ -1,47 +1,60 @@
-defmodule Item, do: defstruct name: nil, sell_in: nil, quality: nil
+defmodule Item do
+  @fields name: nil, sell_in: nil, quality: nil
+  defstruct @fields
+
+  def fields, do: @fields
+end
+
+defmodule Item.Legendary, do: defstruct Item.fields
+defmodule Item.Aged,      do: defstruct Item.fields
+defmodule Item.Backstage, do: defstruct Item.fields
+defmodule Item.Conjured,  do: defstruct Item.fields
+defmodule Item.Normal,    do: defstruct Item.fields
 
 defmodule GildedRose do
-  @sulfuras  "Sulfuras, Hand of Ragnaros"
-  @aged_brie "Aged Brie"
-  @backstage "Backstage passes to a TAFKAL80ETC concert"
-  @conjured  "Conjured Mana Cake"
-
   def update_quality(items) do
     Enum.map( items, &update_item(&1) )
   end
 
   defp update_item(item) do
     item
+    |> typecast
     |> update_item_sell_in
     |> update_item_quality
     |> enforce_quality_constraints
   end
 
-  defp update_item_sell_in( item = %Item{ name: @sulfuras } ) do
+  defp typecast( item = %Item{ name: "Sulfuras, Hand of Ragnaros" } ),                do: %{ item | __struct__: Item.Legendary }
+  defp typecast( item = %Item{ name: "Aged Brie" } ),                                 do: %{ item | __struct__: Item.Aged      }
+  defp typecast( item = %Item{ name: "Backstage passes to a TAFKAL80ETC concert" } ), do: %{ item | __struct__: Item.Backstage }
+  defp typecast( item = %Item{ name: "Conjured Mana Cake" } ),                        do: %{ item | __struct__: Item.Conjured  }
+  defp typecast( item = %Item{} ),                                                    do: %{ item | __struct__: Item.Normal    }
+
+  defp update_item_sell_in( item = %Item.Legendary{} ) do
     item
   end
-  defp update_item_sell_in( item = %Item{ sell_in: sell_in } ) do
+  defp update_item_sell_in( item = %{ sell_in: sell_in } ) do
     %{item | sell_in: item.sell_in - 1}
   end
 
-  defp update_item_quality( item = %Item{ sell_in: sell_in } ) when sell_in < 0 do
+  defp update_item_quality( item = %{ sell_in: sell_in } ) when sell_in < 0 do
     %{item | quality: item.quality + quality_modifier(item) * 2 }
   end
-  defp update_item_quality( item = %Item{} ) do
+  defp update_item_quality( item = %{} ) do
     %{item | quality: item.quality + quality_modifier(item) }
   end
 
-  defp quality_modifier( item = %Item{ name: @sulfuras} ),                                 do: 0
-  defp quality_modifier( item = %Item{ name: @aged_brie } ),                               do: 1
-  defp quality_modifier( item = %Item{ name: @backstage, sell_in: days } ) when days <  0, do: -item.quality
-  defp quality_modifier( item = %Item{ name: @backstage, sell_in: days } ) when days <  5, do: 3
-  defp quality_modifier( item = %Item{ name: @backstage, sell_in: days } ) when days < 10, do: 2
-  defp quality_modifier( item = %Item{ name: @backstage } ),                               do: 1
-  defp quality_modifier( item = %Item{ name: @conjured } ),                                do: -2
-  defp quality_modifier( item = %Item{} ),                                                 do: -1
+  defp quality_modifier( item = %Item.Legendary{} ),                               do: 0
+  defp quality_modifier( item = %Item.Aged{} ),                                    do: 1
+  defp quality_modifier( item = %Item.Backstage{ sell_in: days } ) when days <  0, do: -item.quality
+  defp quality_modifier( item = %Item.Backstage{ sell_in: days } ) when days <  5, do: 3
+  defp quality_modifier( item = %Item.Backstage{ sell_in: days } ) when days < 10, do: 2
+  defp quality_modifier( item = %Item.Backstage{} ),                               do: 1
+  defp quality_modifier( item = %Item.Conjured{} ),                                do: -2
+  defp quality_modifier( item = %Item.Normal{} ),                                                 do: -1
 
-  defp enforce_quality_constraints(item = %Item{ name: @sulfuras } ),        do: item
-  defp enforce_quality_constraints(item = %Item{ quality: q } ) when q > 50, do: %{ item | quality: 50 }
-  defp enforce_quality_constraints(item = %Item{ quality: q } ) when q <  0, do: %{ item | quality:  0 }
-  defp enforce_quality_constraints(item = %Item{} ),                         do: item
+  defp enforce_quality_constraints(item = %Item.Legendary{} ),           do: item
+  defp enforce_quality_constraints(item = %{ quality: q } ) when q > 50, do: %{ item | quality: 50 }
+  defp enforce_quality_constraints(item = %{ quality: q } ) when q <  0, do: %{ item | quality:  0 }
+  defp enforce_quality_constraints(item = %{} ),                         do: item
 end
